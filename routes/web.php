00e829c0280bb229264dd6c5c4732306658957a9
HollowMonarch24 <143695363+HollowMonarch24@users.noreply.github.com>
2025-10-29 16:10:17 +0800

    Updated web.php

--- routes/web.php
+++ routes/web.php
@@ -3,68 +3,41 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\RoleController;    
+use App\Http\Controllers\RoleController;
 use App\Http\Controllers\RegistrationCodeController;
 use App\Http\Controllers\ChildController;
 use App\Http\Controllers\HealthlogController;
 
+Route::get('/', fn() => Inertia::render('home'))->name('home');
+Route::get('/announcements', fn() => Inertia::render('announcements'))->name('announcements');
+Route::get('/contact', fn() => Inertia::render('contact'))->name('contact');
 
-
-// Guest Pages
-Route::get('/', function () {
-    return Inertia::render('home');
-})->name('home');
-
-Route::get('/announcements', function () {
-    return Inertia::render('announcements');
-})->name('announcements');
-
-Route::get('/contact', function () {
-    return Inertia::render('contact');
-})->name('contact');
-
-// Routes for authenticated users
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', fn() => Inertia::render('dashboard'))->name('dashboard');
+    Route::get('/dashboard', fn() => Inertia::render('dashboard'))->name('dashboard');
+
     Route::resource('children', ChildController::class);
     Route::post('/children/{child}/notes', [ChildController::class, 'storeNote'])->name('children.notes.store');
-    
     Route::delete('/children/{child}/notes/{note}', [ChildController::class, 'destroyNote'])->name('children.notes.destroy');
 
-    
-
-
-    //Admin
-    Route::middleware(['role:Admin'])->group(function () {
+    Route::middleware(['role:admin'])->group(function () {
         Route::get('/users/archived', [UserController::class, 'archived'])->name('users.archived');
         Route::post('/users/{id}/restore', [UserController::class, 'restore'])->name('users.restore');
         Route::delete('/users/{id}/force-delete', [UserController::class, 'forceDelete'])->name('users.forceDelete');
         Route::post('/users/{id}/update-role', [UserController::class, 'updateRole'])->name('users.updateRole');
         Route::post('/registration-codes/generate', [RegistrationCodeController::class, 'generate']);
         Route::get('/registration-codes/latest', [RegistrationCodeController::class, 'latest']);
-
-
-
         Route::resource('users', UserController::class);
         Route::resource('roles', RoleController::class);
     });
 
-
-Route::middleware(['auth', 'role:Healthworker'])->group(function () {
-    Route::get('/healthlog', [HealthlogController::class, 'index'])->name('healthlog.index');
-    Route::get('/healthlog/create', [HealthlogController::class, 'create'])->name('healthlog.create');
-    Route::post('/healthlog', [HealthlogController::class, 'store'])->name('healthlog.store');
-    Route::get('/healthlog/{id}', [HealthlogController::class, 'show'])->name('healthlog.show');
-    Route::delete('/healthlog/{id}', [HealthlogController::class, 'destroy'])->name('healthlog.destroy');
-
-    Route::resource('healthlogs', HealthlogController::class);
-     Route::resource('roles', RoleController::class);
-     Route::resource('users', UserController::class);
-
-});
-
-
-
+    Route::middleware(['role:healthworker'])->group(function () {
+        Route::get('/healthlog', [HealthlogController::class, 'index'])->name('healthlog.index');
+        Route::get('/healthlog/create', [HealthlogController::class, 'create'])->name('healthlog.create');
+        Route::post('/healthlog', [HealthlogController::class, 'store'])->name('healthlog.store');
+        Route::get('/healthlog/{id}', [HealthlogController::class, 'show'])->name('healthlog.show');
+        Route::delete('/healthlog/{id}', [HealthlogController::class, 'destroy'])->name('healthlog.destroy');
+        Route::resource('healthlogs', HealthlogController::class);
+    });
 });
 
 require __DIR__.'/settings.php';
