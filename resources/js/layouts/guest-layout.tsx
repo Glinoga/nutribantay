0f8dde52dbfe7e91ea3e3f6673284a73ad6e9495
Ed Jerome Manalo <manaloedjerome@gmail.com>
2026-05-19 15:46:56 +0800

    fix: update font imports to use Bunny.net for consistency

--- resources/js/layouts/guest-layout.tsx
+++ resources/js/layouts/guest-layout.tsx
@@ -13,9 +13,9 @@ export default function GuestLayout({ children, title, showHeader = true }: Prop
     return (
         <>
             <Head title={title}>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.bunny.net" />
                 <link
-                    href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400&display=swap"
+                    href="https://fonts.bunny.net/css?family=montserrat:400,500,600,700,800"
                     rel="stylesheet"
                 />
                 <style>{`
--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -8,9 +8,9 @@ export default function Welcome() {
     return (
         <>
             <Head title="Welcome">
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.bunny.net" />
                 <link
-                    href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400&display=swap"
+                    href="https://fonts.bunny.net/css?family=montserrat:400,500,600,700,800"
                     rel="stylesheet"
                 />
                 <style>{`
